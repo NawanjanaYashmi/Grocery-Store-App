87516d24d1594c62db3af62ddd2e76084de531dd
IT21061134 <nawanjanayeshmi@gmail.com>
2022-12-23 10:38:33 +0530

    update Categorieswidget

--- lib/widgets/CategoriesWidget.dart
+++ lib/widgets/CategoriesWidget.dart
@@ -9,7 +9,37 @@ class CategoriesWidget extends StatelessWidget {
           padding: EdgeInsets.only(left: 10, right: 10, bottom: 10),
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [],
+            children: [
+              Text(
+                "Category",
+                style: TextStyle(
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold,
+                  color: Color.fromARGB(255, 243, 136, 93),
+                ),
+              ),
+              Text(
+                "See All",
+                style: TextStyle(
+                  fontSize: 15,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black54,
+                ),
+              ),
+            ],
+          ),
+        ),
+        SingleChildScrollView(
+          //so row can be scrollable in horizontal
+          scrollDirection: Axis.horizontal,
+          child: Row(
+            children: [
+              //for(int i=1; i<8; i++)
+              Container(
+                margin: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                height: 50,
+              ),
+            ],
           ),
         ),
       ],
