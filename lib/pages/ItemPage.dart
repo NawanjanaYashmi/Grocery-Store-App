13b5447da7ab70bd962f7d912811608e8b9fe2ac
IT21061134 <nawanjanayeshmi@gmail.com>
2023-01-05 21:58:10 +0530

    update

--- lib/pages/ItemPage.dart
+++ lib/pages/ItemPage.dart
@@ -9,7 +9,61 @@ class ItemPage extends StatelessWidget {
         child: SingleChildScrollView(
           child: Column(
             children: [
-              Container(),
+              Container(
+                margin: EdgeInsets.all(15),
+                height: 350,
+                width: double.infinity,
+                alignment: Alignment.topLeft,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage("images/1.png"),
+                  ),
+                ),
+                child: InkWell(
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                  child: Icon(
+                    Icons.arrow_back,
+                    size: 30,
+                    color: Color.fromARGB(255, 243, 136, 93),
+                  ),
+                ),
+              ),
+              Container(
+                //You can change height according to your choice
+                height: MediaQuery.of(context).size.height * 0.4,
+                margin: EdgeInsets.only(top: 20),
+                padding: EdgeInsets.all(15),
+                decoration: BoxDecoration(
+                  color: Color.fromARGB(255, 243, 136, 93),
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(30),
+                    topRight: Radius.circular(30),
+                  ),
+                ),
+                child: Column(
+                  //crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(top: 10, bottom: 15),
+                      child: Row(
+                        //mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            "Fruit Title",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 28,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ],
           ),
         ),
