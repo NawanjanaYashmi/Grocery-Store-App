4cc75c45912f2a15cbad09cef37761c0c6c25775
IT21061134 <nawanjanayeshmi@gmail.com>
2023-01-07 12:24:50 +0530

    update

--- lib/widgets/BottomCartSheet.dart
+++ lib/widgets/BottomCartSheet.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class BottomCartSheet extends StatelessWidget {
@@ -15,67 +16,120 @@ class BottomCartSheet extends StatelessWidget {
               child: SingleChildScrollView(
                 child: Column(
                   children: [
-                    //for(int i=1; i<8; i++)
-                    Container(
-                      margin:
-                          EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10),
-                          color: Colors.white,
-                          boxShadow: [
-                            BoxShadow(
-                                color: Colors.grey.withOpacity(0.5),
-                                spreadRadius: 1,
-                                blurRadius: 8),
-                          ]),
-                      child: Row(
-                        children: [
-                          Padding(
-                            padding: EdgeInsets.all(10),
-                            child: Image.asset(
-                              "images/1.png",
-                              height: 80,
-                              width: 80,
+                    for (int i = 1; i < 4; i++)
+                      Container(
+                        margin:
+                            EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            color: Colors.white,
+                            boxShadow: [
+                              BoxShadow(
+                                  color: Colors.grey.withOpacity(0.5),
+                                  spreadRadius: 1,
+                                  blurRadius: 8),
+                            ]),
+                        child: Row(
+                          children: [
+                            Padding(
+                              padding: EdgeInsets.all(10),
+                              child: Image.asset(
+                                "images/$i.png",
+                                height: 80,
+                                width: 80,
+                              ),
                             ),
-                          ),
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Container(
-                                margin: EdgeInsets.only(bottom: 15),
-                                child: Text(
-                                  "Item Title",
-                                  style: TextStyle(
-                                    color: Color.fromARGB(255, 243, 136, 93),
-                                    fontSize: 22,
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                                ),
-                              ),
-                              Container(
-                                child: Text(
-                                  "\$20",
-                                  style: TextStyle(
-                                    color: Color.fromARGB(255, 243, 136, 93),
-                                    fontSize: 18,
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ),
-                          Spacer(),
-                          Padding(
-                            padding: EdgeInsets.symmetric(horizontal: 10),
-                            child: Column(
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
                               children: [
-                                Icon(Icons.disabled_by_default),
+                                Container(
+                                  margin: EdgeInsets.only(bottom: 15),
+                                  child: Text(
+                                    "Item Title",
+                                    style: TextStyle(
+                                      color: Color.fromARGB(255, 243, 136, 93),
+                                      fontSize: 22,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                ),
+                                Container(
+                                  child: Text(
+                                    "\$20",
+                                    style: TextStyle(
+                                      color: Color.fromARGB(255, 243, 136, 93),
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                ),
                               ],
                             ),
-                          ),
-                        ],
+                            Spacer(),
+                            Padding(
+                              padding: EdgeInsets.symmetric(horizontal: 10),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.end,
+                                children: [
+                                  Icon(
+                                    Icons.disabled_by_default,
+                                    color: Color.fromARGB(255, 243, 136, 93),
+                                    size: 28,
+                                  ),
+                                  SizedBox(height: 25),
+                                  Row(
+                                    children: [
+                                      Container(
+                                        padding: EdgeInsets.all(2),
+                                        decoration: BoxDecoration(
+                                          color: Colors.white,
+                                          borderRadius:
+                                              BorderRadius.circular(20),
+                                          boxShadow: [
+                                            BoxShadow(
+                                                color: Colors.grey
+                                                    .withOpacity(0.5),
+                                                spreadRadius: 2,
+                                                blurRadius: 8),
+                                          ],
+                                        ),
+                                        child: Icon(CupertinoIcons.minus),
+                                      ),
+                                      Container(
+                                        margin: EdgeInsets.symmetric(
+                                            horizontal: 10),
+                                        child: Text(
+                                          "01",
+                                          style: TextStyle(
+                                            fontSize: 18,
+                                            fontWeight: FontWeight.bold,
+                                          ),
+                                        ),
+                                      ),
+                                      Container(
+                                        padding: EdgeInsets.all(2),
+                                        decoration: BoxDecoration(
+                                          color: Colors.white,
+                                          borderRadius:
+                                              BorderRadius.circular(20),
+                                          boxShadow: [
+                                            BoxShadow(
+                                                color: Colors.grey
+                                                    .withOpacity(0.5),
+                                                spreadRadius: 2,
+                                                blurRadius: 8),
+                                          ],
+                                        ),
+                                        child: Icon(CupertinoIcons.plus),
+                                      ),
+                                    ],
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
-                    ),
                   ],
                 ),
               ),
